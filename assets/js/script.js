3e4be8725716e39c3487a0af2cb569f3efbf1a1b
Hannah Johnson <hgrace99@gmail.com>
2022-08-31 19:58:51 -0500

    added timer function, questions and score html pages

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+// questions
+var codeQuestions = [
+    {
+        id: 0,
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+        id: 1,
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	},
+    {
+        id: 2,
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
+
+
+
+// timer function
+
+var timeLeft = 60;
+    var elem = document.getElementById('timer');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        elem.innerHTML = 'Time: ' + timeLeft ;
+        timeLeft--;
+      }
+    }
+
